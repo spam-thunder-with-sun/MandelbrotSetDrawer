3fb584d6fe70170359385f7ad8117a7b8e335930
Stefano <stefano.faccio@studenti.unitn.it>
2023-03-23 23:37:21 +0100

    Aggiungere i file di progetto.

---
+++ InsiemeMandelbrot/FormMain.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace InsiemeMandelbrot
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDisegna = new System.Windows.Forms.Button();
+            this.pnl = new System.Windows.Forms.Panel();
+            this.pnl_Scelta = new System.Windows.Forms.Panel();
+            this.rdOttima = new System.Windows.Forms.RadioButton();
+            this.rdBuona = new System.Windows.Forms.RadioButton();
+            this.rdScarsa = new System.Windows.Forms.RadioButton();
+            this.lbl_3 = new System.Windows.Forms.Label();
+            this.btnRidisegna = new System.Windows.Forms.Button();
+            this.pnl_Scelta.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnDisegna
+            // 
+            this.btnDisegna.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnDisegna.Location = new System.Drawing.Point(307, 12);
+            this.btnDisegna.Name = "btnDisegna";
+            this.btnDisegna.Size = new System.Drawing.Size(87, 31);
+            this.btnDisegna.TabIndex = 0;
+            this.btnDisegna.Text = "Disegna!";
+            this.btnDisegna.UseVisualStyleBackColor = false;
+            this.btnDisegna.Click += new System.EventHandler(this.btnDisegna_Click);
+            // 
+            // pnl
+            // 
+            this.pnl.Location = new System.Drawing.Point(12, 49);
+            this.pnl.Name = "pnl";
+            this.pnl.Size = new System.Drawing.Size(1200, 800);
+            this.pnl.TabIndex = 1;
+            // 
+            // pnl_Scelta
+            // 
+            this.pnl_Scelta.Controls.Add(this.rdOttima);
+            this.pnl_Scelta.Controls.Add(this.rdBuona);
+            this.pnl_Scelta.Controls.Add(this.rdScarsa);
+            this.pnl_Scelta.Controls.Add(this.lbl_3);
+            this.pnl_Scelta.Location = new System.Drawing.Point(12, 12);
+            this.pnl_Scelta.Name = "pnl_Scelta";
+            this.pnl_Scelta.Size = new System.Drawing.Size(279, 31);
+            this.pnl_Scelta.TabIndex = 4;
+            // 
+            // rdOttima
+            // 
+            this.rdOttima.AutoSize = true;
+            this.rdOttima.Location = new System.Drawing.Point(210, 3);
+            this.rdOttima.Name = "rdOttima";
+            this.rdOttima.Size = new System.Drawing.Size(55, 17);
+            this.rdOttima.TabIndex = 4;
+            this.rdOttima.Text = "Ottima";
+            this.rdOttima.UseVisualStyleBackColor = true;
+            // 
+            // rdBuona
+            // 
+            this.rdBuona.AutoSize = true;
+            this.rdBuona.Checked = true;
+            this.rdBuona.Location = new System.Drawing.Point(149, 3);
+            this.rdBuona.Name = "rdBuona";
+            this.rdBuona.Size = new System.Drawing.Size(56, 17);
+            this.rdBuona.TabIndex = 3;
+            this.rdBuona.TabStop = true;
+            this.rdBuona.Text = "Buona";
+            this.rdBuona.UseVisualStyleBackColor = true;
+            // 
+            // rdScarsa
+            // 
+            this.rdScarsa.AutoSize = true;
+            this.rdScarsa.Location = new System.Drawing.Point(87, 3);
+            this.rdScarsa.Name = "rdScarsa";
+            this.rdScarsa.Size = new System.Drawing.Size(58, 17);
+            this.rdScarsa.TabIndex = 2;
+            this.rdScarsa.Text = "Scarsa";
+            this.rdScarsa.UseVisualStyleBackColor = true;
+            // 
+            // lbl_3
+            // 
+            this.lbl_3.AutoSize = true;
+            this.lbl_3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_3.Location = new System.Drawing.Point(4, 0);
+            this.lbl_3.Name = "lbl_3";
+            this.lbl_3.Size = new System.Drawing.Size(63, 20);
+            this.lbl_3.TabIndex = 0;
+            this.lbl_3.Text = "Qualità:";
+            // 
+            // btnRidisegna
+            // 
+            this.btnRidisegna.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnRidisegna.Location = new System.Drawing.Point(400, 12);
+            this.btnRidisegna.Name = "btnRidisegna";
+            this.btnRidisegna.Size = new System.Drawing.Size(87, 31);
+            this.btnRidisegna.TabIndex = 5;
+            this.btnRidisegna.Text = "Ridisegna";
+            this.btnRidisegna.UseVisualStyleBackColor = false;
+            this.btnRidisegna.Click += new System.EventHandler(this.btnRidisegna_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(1234, 860);
+            this.Controls.Add(this.btnRidisegna);
+            this.Controls.Add(this.pnl_Scelta);
+            this.Controls.Add(this.pnl);
+            this.Controls.Add(this.btnDisegna);
+            this.Name = "FormMain";
+            this.Text = "Insieme di Mandelbrot";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.pnl_Scelta.ResumeLayout(false);
+            this.pnl_Scelta.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDisegna;
+        private System.Windows.Forms.Panel pnl;
+        private System.Windows.Forms.Panel pnl_Scelta;
+        private System.Windows.Forms.RadioButton rdOttima;
+        private System.Windows.Forms.RadioButton rdBuona;
+        private System.Windows.Forms.RadioButton rdScarsa;
+        private System.Windows.Forms.Label lbl_3;
+        private System.Windows.Forms.Button btnRidisegna;
+    }
+}
+
---
+++ InsiemeMandelbrot/FormMain.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InsiemeMandelbrot
+{
+    public partial class FormMain : Form
+    {
+        const int MAX = 1000;
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        int Gpenna, Npixel, n34, n12, n14;//3/4 di Npixel,2/4 di Npixel ,1/4 di Npixel; 
+        Thread[] t = new Thread[8];
+        Bitmap[] b = new Bitmap[8];
+
+        private void btnRidisegna_Click(object sender, EventArgs e)
+        {
+            AggiornaGrafica(null, null);
+        }
+
+        private void btnDisegna_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 8; i++)
+                b[i] = null;
+
+            if (rdScarsa.Checked == true)
+                Npixel = 80;
+            else if (rdBuona.Checked == true)
+                Npixel = 200;
+            else
+                Npixel = 400;
+
+            Gpenna = 400 / Npixel;
+
+            //Mi salvo le unità perchè le uso spesso in seguito
+            n14 = Npixel >> 2;
+            n12 = Npixel >> 1;
+            n34 = ((Npixel << 1) + Npixel) >> 2;//moltiplico per 2, aggiungo 1 unità (moltiplicato per 3), divido per 4
+
+            btnRidisegna.Visible = true;
+            SmistaTread(); //Calcolo della frattale
+        }
+
+        void SmistaTread()
+        {
+            for (int i = 0; i < 8; i++)
+                t[i] = null;
+
+            t[0] = new Thread(T1);
+            t[1] = new Thread(T2);
+            t[2] = new Thread(T3);
+            t[3] = new Thread(T4);
+            t[4] = new Thread(T5);
+            t[5] = new Thread(T6);
+            t[6] = new Thread(T7);
+            t[7] = new Thread(T8);
+
+            for (int i = 0; i < 8; i++)
+                t[i].Start();
+
+            for (int i = 0; i < 8; i++)
+                t[i].Join(Timeout.Infinite);
+
+            for (int i = 0; i < 8; i++)
+                t[i] = null;
+
+            AggiornaGrafica(null, null);
+            return;
+        }
+
+        private void AggiornaGrafica(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 8; i++)
+                if (b[i] != null)
+                    pnl.CreateGraphics().DrawImage(b[i], 0, 0);
+        }
+
+        void Procedimento(int i, ref Graphics graph)
+        {
+            int x, y, count;
+            Complex c, z;
+            Color colore;
+
+            for (int r = -(Npixel << 1); r < Npixel; r++)
+            {
+                //Calcolo Frattale
+                //Insieme di Mandelbrot
+                //Verisone originale
+                
+                z = new Complex(0, 0);
+                c = new Complex((double)r / (double)Npixel, (double)i / (double)Npixel);
+                count = 0;
+                while (count < MAX && z.Magnitude <= 2)
+                {
+                    z = Complex.Pow(z, 2);
+                    z += c;
+                    ++count;
+                }
+                //Ho calcolato, ora decido il colore
+                if (z.Magnitude <= 2)
+                    colore = Color.FromArgb(0, 0, 0);
+                else
+                {
+                    if (count <= 3)
+                        colore = Color.FromArgb(0, 0, 120);
+                    else if (count <= 6)
+                        colore = Color.FromArgb(0, 0, 200);
+                    else if (count <= 10)
+                        colore = Color.FromArgb(0, 0, 255);
+                    else if (count <= 14)
+                        colore = Color.FromArgb(0, 180, 180);
+                    else if (count <= 20)
+                        colore = Color.FromArgb(0, 120, 0);
+                    else if (count <= 50)
+                        colore = Color.FromArgb(255 ,255, 0);
+                    else if(count <= 100)
+                        colore = Color.FromArgb(255, 102, 0);
+                    else 
+                        colore = Color.FromArgb(255, 0, 0);                    
+                }
+
+                //Calcolo la posizione per disegnare
+                y = (Npixel + 1 - i) * Gpenna; 
+                x = (r + (Npixel << 1)) * Gpenna;
+
+                //Disegno
+                graph.DrawLine(new Pen(colore, Gpenna), new Point(x, y), new Point(x + Gpenna + 1, y));
+            }
+            return;
+        }
+
+        //*****************************************************************
+
+        void T1()
+        {
+            Bitmap b = new Bitmap(pnl.Width, pnl.Height);
+            Graphics g = Graphics.FromImage(b);
+            for (int i = Npixel; i > Npixel - n14; i--)
+                Procedimento(i, ref g);
+            this.b[0] = b;
+            b = null;
+            return;
+        }
+        void T2()
+        {
+            Bitmap b = new Bitmap(pnl.Width, pnl.Height);
+            Graphics g = Graphics.FromImage(b);
+            for (int i = Npixel - n14; i > Npixel - n12; i--)
+                Procedimento(i, ref g);
+            this.b[1] = b;
+            b = null;
+            return;
+        }
+        void T3()
+        {
+            Bitmap b = new Bitmap(pnl.Width, pnl.Height);
+            Graphics g = Graphics.FromImage(b);
+            for (int i = Npixel - n12; i > Npixel - n34; i--)
+                Procedimento(i, ref g);
+            this.b[2] = b;
+            b = null;
+            return;
+        }
+        void T4()
+        {
+            Bitmap b = new Bitmap(pnl.Width, pnl.Height);
+            Graphics g = Graphics.FromImage(b);
+            for (int i = Npixel - n34; i > 0; i--)
+                Procedimento(i, ref g);
+            this.b[3] = b;
+            b = null;
+            return;
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void T5()
+        {
+            Bitmap b = new Bitmap(pnl.Width, pnl.Height);
+            Graphics g = Graphics.FromImage(b);
+            for (int i = 0; i > -n14; i--)
+                Procedimento(i, ref g);
+            this.b[4] = b;
+            b = null;
+            return;
+        }
+        void T6()
+        {
+            Bitmap b = new Bitmap(pnl.Width, pnl.Height);
+            Graphics g = Graphics.FromImage(b);
+            for (int i = -14; i > -n12; i--)
+                Procedimento(i, ref g);
+            this.b[5] = b;
+            b = null;
+            return;
+        }
+        void T7()
+        {
+            Bitmap b = new Bitmap(pnl.Width, pnl.Height);
+            Graphics g = Graphics.FromImage(b);
+            for (int i = -n12; i > -n34; i--)
+                Procedimento(i, ref g);
+            this.b[6] = b;
+            b = null;
+            return;
+        }
+        void T8()
+        {
+            Bitmap b = new Bitmap(pnl.Width, pnl.Height);
+            Graphics g = Graphics.FromImage(b);
+            for (int i = -n34; i > -Npixel; i--)
+                Procedimento(i, ref g);
+            this.b[7] = b;
+            b = null;
+            return;
+        }
+    }
+}
+
